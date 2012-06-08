e6203e3f1f1337256711d7571151c91f7f3d47a1
kanji2012 <kanjilal.kaos07@gmail.com>
2012-06-08 16:52:31 +0530

    updated code

--- KanjiSpider.py
+++ KanjiSpider.py
@@ -3,6 +3,7 @@ import sqlite3
 from collections import deque
 import sgmllib
 import sys
+import os
 
 q = deque([])         # This is a queue to store the urls as BFS algorithm is used here.
 
@@ -51,9 +52,13 @@ def crawl():
     crawl() is the first method gets called when the module is run.The first url given by the user will get stored into the database by calling the insert_val() method and depending on the returned value call() method gets called.After that the first element of the queue is popped out and again insert_val() method gets called with that element i.e. the url.
 
     '''
-    connection, cursor=openDB()
-    #createDB(connection,cursor)
-    resetDB(connection,cursor)
+    #connection, cursor=openDB()
+    if os.path.exists("./mydb"):
+        connection, cursor=openDB()
+        resetDB(connection,cursor)
+    else:
+        connection, cursor=openDB()
+        createDB(connection,cursor)
     u = raw_input("Enter a URL: ")
     x, u = insert_val(u,connection,cursor)
     while True:
